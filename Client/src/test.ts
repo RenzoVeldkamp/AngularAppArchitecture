eabff93776adbf7eefca4980ae26c5f91a652e29
Renzo Veldkamp <RenzoVeldkamp@users.noreply.github.com>
2025-07-21 17:50:51 +0200

    wip on unit test fixing

--- Client/src/test.ts
+++ Client/src/test.ts
@@ -1,16 +1,25 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     <T>(id: string): T;
     keys(): string[];
   };
 };
 
 // First, initialize the Angular testing environment.
+TestBed.initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
 /*getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
